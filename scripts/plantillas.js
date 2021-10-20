028a9ad7e7377d3c6dec14a3f8cca8601a5fb32a
María <magomez294@gmail.com>
2021-10-20 10:09:47 +0200

    feature_Maria

--- scripts/plantillas.js
+++ scripts/plantillas.js
@@ -1,12 +1,21 @@
-function guardar (e) {
-e.preventDefault();
-    mostrarDatos ()
+let date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+document.getElementById("fecManual").value = `${year}-${month}-${day}`;
+
+
+function guardar () {
+    localStorage.setItem('fecManual', document.getElementById("fecManual").value);
+    localStorage.setItem('nomPersona', document.getElementById("nomPersona").value);
+    localStorage.setItem('estudioActual', document.getElementById("estudioActual").value);
+    localStorage.setItem('lugar', document.getElementById("lugar").value);
+    localStorage.setItem('ages', document.getElementById("ages").value);
+    localStorage.setItem('numTelf', document.getElementById("numTelf").value);
+    localStorage.setItem('corElectronico', document.getElementById("corElectronico").value);
+    localStorage.setItem('imgPrincipal', document.getElementById("imgPrincipal").value);
+    localStorage.setItem('nomDispositivo', document.getElementById("dispositivo").value);
+    localStorage.setItem('parteReemplazada', document.getElementById("infoRemplazo").value);
 }
-function mostrarDatos () {
-    var nombre = document.getElementById("nomPersona");
-    var element = document.getElementById("lugarNombre");
-    console.log(element);
-    console.log(nombre);
-    element.innerHTML = nombre.value;
-    console.log(element);
-}
+
